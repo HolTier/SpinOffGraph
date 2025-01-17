5b6ef567d641b59bdb48eee0d7b43effaae46990
Marcin Holcman <62081203+HolTier@users.noreply.github.com>
2025-01-17 13:02:26 +0100

    Add unit tests for MediaService with Moq
    Updated Media.Tests.csproj to include Moq package for mocking.
    Added necessary using directives in MediaServiceTests.cs.
    Introduced Media.Tests.Services namespace with MediaServiceTests class.
    Initialized mock objects and MediaService instance in constructor.

---
+++ Backend/Services/Media.Tests/Services/MediaServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using Media.Api.Data;
+using Media.Api.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Media.Tests.Services
+{
+    public class MediaServiceTests
+    {
+        private readonly Mock<IMediaRepository> _mediaRepositoryMock;
+        private readonly Mock<IMediaTypeRepository> _mediaTypeRepositoryMock;
+        private readonly MediaService _mediaService;
+
+        public MediaServiceTests()
+        {
+            _mediaRepositoryMock = new Mock<IMediaRepository>();
+            _mediaTypeRepositoryMock = new Mock<IMediaTypeRepository>();
+            _mediaService = new MediaService(_mediaRepositoryMock.Object, _mediaTypeRepositoryMock.Object);
+        }
+
+
+    }
+}
