202b17695b08a35efe4d2b0532c733655274c8c7
Marcin Holcman <62081203+HolTier@users.noreply.github.com>
2025-01-20 13:13:11 +0100

    Add unit tests for MediaController using Moq
    Introduce MediaControllerTests class in MediaControllerTests.cs.
    This class, part of Media.Tests.Controllers, includes necessary
    using directives and sets up a mock IMediaService for isolated
    testing of MediaController behavior.

---
+++ Backend/Services/Media.Tests/Controllers/MediaControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using Media.Api.Controllers;
+using Media.Api.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Media.Tests.Controllers
+{
+    public class MediaControllerTests
+    {
+        private readonly Mock<IMediaService> _mediaService;
+        private readonly MediaController _mediaController;
+
+        public MediaControllerTests()
+        {
+            _mediaService = new Mock<IMediaService>();
+            _mediaController = new MediaController(_mediaService.Object);
+        }
+
+
+    }
+}
